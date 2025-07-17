39cadfdabdadb269e04ae908d0a200953737d365
Stancu Nicol-Alexia <nicol.stancu@student.unitbv.ro>
2025-07-17 13:25:27 +0300

    I created header for the main page.

--- FootballClient/src/app/app.ts
+++ FootballClient/src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from './home/home';
+import {Header} from './header/header' ;
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, Home, Header],
+  template: `
+  <app-header />
+  <app-home />
+  `
 })
 export class App {
   protected readonly title = signal('FootballClient');
