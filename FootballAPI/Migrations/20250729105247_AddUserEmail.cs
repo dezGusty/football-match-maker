d35bb1309b9708de2613146a912c1b1e236b97ef
DenisTatu <tatu_denis@yahoo.com>
2025-07-29 14:10:16 +0300

    [FIX] - Fixed login using email for the DB

---
+++ FootballAPI/Migrations/20250729105247_AddUserEmail.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FootballAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserEmail : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: true); // <-- permite NULL
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true,
+                filter: "[Email] IS NOT NULL"); // <-- index unic doar pentru valori nenule
+        }
+
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+        }
+
+    }
+}
